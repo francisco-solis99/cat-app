9ec357d52a9acd37dafbfa7db39d4e176188f2a2
francisco-solis99 <franciscosoliscss@gmail.com>
2022-07-30 14:26:10 -0500

    [Add] the js code for upload and preview a cat image

--- src/js/view.js
+++ src/js/view.js
@@ -1,3 +1,7 @@
+// ES6 Modules or TypeScript
+import Swal from 'sweetalert2'
+
+
 export default class KittieView {
   #API;
   constructor() {
@@ -13,17 +17,24 @@ export default class KittieView {
   }
 
   init(){
+    // render the initial random kitties at the beginning
     this.renderRandomKitties(4);
-    const form = this.randomSection.querySelector('.random__form');
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-      const numKitties = Number(e.target.querySelector('.random__input').value);
-      this.randomSection.removeChild(this.randomSection.lastChild);
-      console.log(numKitties);
-      this.renderRandomKitties(numKitties);
-    })
+    // util for select a HTML element
+    const selectHtmlElement = (parent, query) => parent.querySelector(query) ?? null;
+
+    // add listeners for random section
+    const form = selectHtmlElement(this.randomSection, '.random__form');
+    form.addEventListener('submit', (e) => this.handlerSubmitRandom(e));
+
+    // add listeners for upload section
+    const inputFile = selectHtmlElement(this.uploadSection, '.upload__file');
+    const selectFileBtn = selectHtmlElement(this.uploadSection, '.select__button');
+    selectFileBtn.addEventListener('click', () => inputFile.click());
+
+    inputFile.addEventListener('change', (e) => this.handlerPreviewImage(e));
   }
 
+  // main functions
   async renderRandomKitties(numKitties = 1) {
     const div = document.createElement('div');
     div.classList.add('random__kitties-container');
@@ -52,5 +63,52 @@ export default class KittieView {
     const reponse = await fetch(randomUrl);
     const kitties = await reponse.json();
     return kitties;
+  }
+
+
+  // handlers for random section
+  handlerSubmitRandom(e){
+    e.preventDefault();
+    const numKitties = Number(e.target.querySelector('.random__input').value);
+    if(numKitties > 0) {
+      this.randomSection.removeChild(this.randomSection.lastChild);
+      this.renderRandomKitties(numKitties);
+      return;
+    }
+    // send a error with modal
+    Swal.fire({
+      title: 'Error!',
+      text: 'You need to load at least 1 random cat',
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    });
+  }
+
+  handlerPreviewImage(e) {
+    const file = e.target.files[0]; //get the first file (not multiple attribute in file input)
+    if(this.validFileType(file)) {
+      const imgPreview = this.uploadSection.querySelector('.upload__img');
+      const urlImage = URL.createObjectURL(file);
+      imgPreview.classList.add('loaded');
+      imgPreview.src = urlImage;
+      return;
+    }
+    // send a error with modal
+    Swal.fire({
+      title: 'Error!',
+      text: 'The image type has to be .jpg or .png and also teh image has to be a cat',
+      icon: 'error',
+      confirmButtonText: 'Ok'
+    });
+  }
+
+
+  // utils functions
+  validFileType(file) {
+    const fileTypes = [
+      "image/jpeg",
+      "image/png",
+      ];
+    return fileTypes.includes(file.type);
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import KittieView from "./js/view.js";
+
+
 const dynamicView = new KittieView();
 dynamicView.init();
